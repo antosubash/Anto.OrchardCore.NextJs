35305d13a530c1ddaa6e76abab1f8dc82a8ffb9f
Anto Subash <antosubash@live.com>
2021-11-17 12:24:55 +0100

    Create Layout.tsx

---
+++ components/Layout.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {
+    
+}
+
+const Layout = (props: Props) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Layout
